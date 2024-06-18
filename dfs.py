b8deba6d77fc2d3bb5149afdf716d3e4f1855ad6
Luis Enrique Pérez Señalin <luis.perez05@epn.edu.ec>
2024-06-18 01:05:01 +0000

    1.0 proyect EDA2

---
+++ dfs.py
@@ -0,0 +1,32 @@
+def get_dfs_tree(graph, start_node):
+    searched = {}
+    for node in grafo_letras:
+        searched[node] = False
+    componentR = []
+    tree = {}
+    dfs(start_node, graph, searched, componentR, tree)
+    return tree
+
+def dfs(nodo, grafo, searched, componentR, tree):
+    componentR.append(nodo)
+    searched[nodo] = True
+
+    for vecino in grafo[nodo]:
+        if not searched[vecino]:
+            tree[vecino] = nodo
+            dfs(vecino, grafo, searched, componentR,tree)
+
+grafo_letras = {
+    'S': ['A','B','C'],
+    'A': ['D','B'],
+    'B': ['F','E','C'],
+    'C': ['G','H'],
+    'D': ['F'],
+    'F': [ ],
+    'E': ['G'],
+    'G': [ ],
+    'H': []
+}
+
+if(__name__ == "__main__"):
+    get_dfs_tree(grafo_letras, 'S')
